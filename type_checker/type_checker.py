28a1f253a1e4c88d46c18442e808dcc7a301e47e
sizzleru <xorzong@gmail.com>
2021-10-19 07:10:37 +1100

    Added base validators and main decorator

--- type_checker/type_checker.py
+++ type_checker/type_checker.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Dict, Tuple
+from typing import Any, Callable, Dict, Tuple, get_type_hints
 
 RETURN_PARAMETER = "return"
 
@@ -32,3 +32,50 @@ def _get_args(func: Callable[..., Any], args: Tuple[Any], kwargs: Tuple[Any]):
     args_value = _get_input_args(func, args, kwargs)
     args_value[RETURN_PARAMETER] = func(*args, **kwargs)
     return args_value
+
+
+def _validator(func: Callable[..., Any]) -> Callable[..., Any]:
+    """[validates the parameters using the wrapper function validator_wrapper]
+
+    Args:
+        func (Callable[..., Any]): [the subjected function]
+
+    Returns:
+        Callable[..., Any]: [returns a wrapper function that does the validation]
+    """
+
+    def validator_wrapper(*args: Tuple[Any], **kwargs: Tuple[Any]) -> Any:
+        """[validates the values of the parameters of a function against the type hints provided]
+
+        Returns:
+            Any: [the original function return value]
+        """
+        parameter_values = _get_args(func, args, kwargs)
+        parameter_type_hints = get_type_hints(func)
+        return parameter_values[RETURN_PARAMETER]
+
+    return validator_wrapper
+
+
+def _skip_validator(func: Callable[..., Any]) -> Callable[[Any], Any]:
+    """[returns the parameter value, acting as a function that skips the validation process]
+
+    Args:
+        func (Callable[..., Any]): [the subjected function]
+
+    Returns:
+        Callable[[Any], Any]: [the original function]
+    """
+    return func
+
+
+def type_check(run: bool = True) -> Callable[[Callable], Callable]:
+    """[a decorator function for validating input and output parameters]
+
+    Args:
+        run (bool, optional): [can be set to false when the programing is not in debug mode]. Defaults to True.
+
+    Returns:
+        Callable[[Callable], Callable]: [a validator wrapper wrapper function]
+    """
+    return _validator if run else _skip_validator
